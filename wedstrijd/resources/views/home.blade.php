64f5584551218fc2d33f2a302ee5418f179f1b96
ajkea <hendrikvd@msn.com>
2017-11-02 21:46:20 +0100

    add homepage + layout + question page

---
+++ wedstrijd/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+    <section class="content-1 homepage">
+        <div class="container">
+            <div class="row justify-center">
+            <div class="col-md-12 text-center">
+                <h1>Competition time baby</h1>
+        </div>
+                <div class="col-md-6 text-center">
+                    <img class="mb-4 img-fluid" src="https://i.imgur.com/5xthSad.png" >
+                </div>
+                <div class="col-md-6 text-center text-md-left" >
+                    <h2 class="mb-4 mt-4">Win an exclusive meeting with Kanye West</h2>
+                    <p class="mb-4">As music enthousiast we like to share the possibility for everyone to visit their biggest stars. This time we bring you the one and only Kanye West!</p>
+                    <p><a class="btn btn-outline-secondary" href="#" role="button">Click here to enter</a></p>
+                    <p>Thanks and goodluck from us at <a class="spotify" href="www.spotify.com">Spotify <i class="fab fa-spotify"></i></a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ wedstrijd/resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <!-- <meta http-equiv="refresh" content="3" > -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+    <!-- Scripts font awesome -->
+    <script defer src="{{ asset('js/fontawesome.js') }}"></script>
+    <script defer src="{{ asset('js/light.js') }}"></script>
+    <script defer src="{{ asset('js/regular.js') }}"></script>
+    <script defer src="{{ asset('js/solid.js') }}"></script>
+    <script defer src="{{ asset('js/brands.js') }}"></script>
+
+</head>
+<body>
+    <div id="app">
+    <div class="container">
+        @yield('content')
+        </div>
+    </div>
+
+    <!--Scripts-->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+</html>
